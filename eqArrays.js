a4164385ed548244039b77f6c2416e842320d7e0
Christian Girling <girling.christian@gmail.com>
2022-01-11 18:03:59 -0800

    Created function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === arr2) return true;
+  if (arr1 == null || arr2 == null) return false
+  if (arr1.length !== arr2.length) return false
+
+  for (let i = 0; i < arr1.length - 1; i++) {
+    if (arr1[i] !== arr2[i]) return false
+  }
+  return true
+}
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(['boom', 3, true], ['boom', 3, true]), true) // => should PASS
+assertEqual(eqArrays(['kaboom', 3, true], ['boom', 3, true]), true) // => should FAIL
