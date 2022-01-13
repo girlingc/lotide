1dc6d0b5594eedef7185e2107f7526fdb15132de
Christian Girling <girling.christian@gmail.com>
2022-01-12 21:29:24 -0800

    Syntax changes

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,12 +9,12 @@ const assertEqual = function(actual, expected) {
 const findKeyByValue = function(object, str) {
   for (const [key, value] of Object.entries(object)) {
     if (value === str) {
-      return `${key}`
+      return `${key}`;
     }
   }
-}
+};
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
@@ -22,7 +22,7 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, undefined), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Good Wife"), "drama")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Good Wife"), "drama");
