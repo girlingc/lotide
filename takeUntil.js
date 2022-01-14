ee66f42296b1c52f9c8a30efd61327f89198beb5
Christian Girling <girling.christian@gmail.com>
2022-01-13 17:24:43 -0800

    Created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === arr2) return true;
+  if (arr1 == null || arr2 == null) return false;
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+//----------------------------------------------------------------
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      results.push(item);
+    }
+  } return results;
+};
+
+
+const data1 = [1, 2, 5, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+assertArraysEqual(results1, [1, 2, 5, 2]);
+
+console.log('--------------');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
