212a7f86820cc20ca49f9375e2ce346118886acc
Christian Girling <girling.christian@gmail.com>
2022-01-17 17:22:41 -0800

    made changes so that assertEqual can be exported

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+
+let str1 = 'hello'
+let str2 = 'hello'
+
+assertEqual(str1, str2)
+
+module.exports = assertEqual
---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual')
+const head = require('../head')
+
+const array = [1,2,3,4,5]
+
+assertEqual(head(array), 1)
