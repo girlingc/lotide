b724296b578a446cf80a75dfd281240424811a1a
Christian Girling <girling.christian@gmail.com>
2022-01-17 17:27:21 -0800

    Made test for tail and refactored.

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const tail = function(arr) {
   let arrTail = arr.slice(1);
   return arrTail
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
+
