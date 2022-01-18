2e593c4b0f9e4f3b1d4e36e51369482385d88ff2
Christian Girling <girling.christian@gmail.com>
2022-01-17 18:51:10 -0800

    Made middleTest using Mocha Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,13 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
+const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 
-assertArraysEqual(middle(['no', 'no', 'middle', 'no', 'no']), ['middle']); // should PASS
-assertArraysEqual(middle(['no', 'no', 'middle1', 'middle2', 'no', 'no']), ['middle1', 'middle2']); // should PASS
-assertArraysEqual(middle(['no', 'no']), []); // should PASS
+describe('#middle', () => {
+  it("should return ['middle'] for ['no', 'middle', 'no']", () => {
+    assert.deepEqual(middle(['no', 'middle', 'no']), ['middle']);
+  });
+  it("should return ['middle1', middle2'] for ['no', 'no', 'middle1', 'middle2', 'no', 'no']", () => {
+    assert.deepEqual(middle(['no', 'no', 'middle1', 'middle2', 'no', 'no']), ['middle1', 'middle2']);
+  });
+});
+
